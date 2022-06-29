2029b5f469874678de5d3291a44fbf729c57c649
thanhleothanh <thanhng260588@gmail.com>
2022-06-29 10:54:09 +0700

    project init

---
+++ src/main/java/com/ghtk/onlinebiddingproject/OnlinebiddingprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ghtk.onlinebiddingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebiddingprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebiddingprojectApplication.class, args);
+	}
+
+}
